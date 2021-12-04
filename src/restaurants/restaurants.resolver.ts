eba2bcf01df23c3c05c0059e03a824a78e155501
Unchaptered <workstation19961002@gmail.com>
2021-12-04 20:54:05 +0900

    #1.3 Arguments
    GraphQL 에서 args 를 요청 받으려면 이를 선언해두어야 한다.
    Nest-GrpahQL 에서 Resolver.TS 파일 안에서 Args를 요청 받고 싶은 Query 나 Mutation 에 다음 코드를 넣어보자.
    (@Args("argsName") argsName: argsType)

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,14 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
 import { Restaurant } from "./entities/restaurant.entitiy";
 
 @Resolver(()=>Restaurant)
 export class RestaurantResolver {
-    @Query(()=>Restaurant)
-    myRestaurant():boolean {
-        return true;
+    @Query(()=>[Restaurant])
+    restaurants(@Args("veganOnly") veganOnly: boolean):Restaurant[]{
+        console.log(veganOnly);
+        return [];
     }
+    /* @Args("veganOnly") 는 GraphQL 의 Args 선언이고
+        veganOnly:boolean 이 TS-entity 를 위한 Args 선언이다.
+    */
 }
