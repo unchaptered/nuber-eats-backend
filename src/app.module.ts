024fa0493fa71bf85f5d17c57c9870baddb0eba5
Unchaptered <workstation19961002@gmail.com>
2021-12-04 22:57:30 +0900

    #2.6 Validating ConfigService
    Joi 는 "Javascript"로 만들어진 가장 강력한 스키마 설명 언어이자, 환경변수 등의 유효성 검사를 할 수 있습니다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,11 @@ import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+// Import JS Module without TS
+import * as Joi from "joi";
+
 import { RestaurantsModule } from "./restaurants/restaurants.module";
+
 
 @Module({
   imports: [
@@ -11,6 +15,14 @@ import { RestaurantsModule } from "./restaurants/restaurants.module";
       isGlobal:true,
       envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
       ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev','prod'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
