337521324af9a419122fbcf55b007d46f8fed8bd
Unchaptered <workstation19961002@gmail.com>
2021-12-04 20:22:41 +0900

    #1.1 Our First Resolver
    app.module.ts 의 GraphQLModule.forRoot({}) 에 1,2 중 하나의 코드를 추가하면 에러 메세지를 반환 받는다. (
    각 코드의 차이는 app.module.ts 에 주석처리 되어있다.)
    1. autoSchemaFile:ture
    2. autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
    에러 메세지
    GraphQLError [Object]: Query root type must be provided.
    우리는 GraphQL Server 을 사용하기 위해서는 Query (Mutation) 와 Resolvers 를 만들어야 한다.
    Nest JS 에서는 이를 작성하고 적용하는 방법은 다음이 있다.
    1. gql 파일로 작성하는 것
    2. ts 파일로 작성하는 것
    이번에는 ts 파일로 작성하여 에러를 해결하였다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      /* GrpahQL Schema 파일을 메모리에 보관할지 경롯 상에 생성할지를 결정짓는다.
+        true 로하면 메모리 상에서만 보관하고
+        join() 을 이용하면 지정한 경로 상에 생성한다.
+      */
+    }),
+    RestaurantsModule
+  ],
   controllers: [],
   providers: [],
 })
