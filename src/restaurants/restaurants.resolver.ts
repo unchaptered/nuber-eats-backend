38b6bce4bc34aec6afe43bd151d8d0aa3d22b337
Unchaptered <workstation19961002@gmail.com>
2021-12-04 21:29:34 +0900

    #1.4 Args without InputTypes
    InputType 없이 복수의 Args를 넘기는 방법
    아래 두 파일을 참고하자.
    sr\restaurant\restaurants.resolver.ts
    sr\restaurant\entities\restaurant.entity.ts

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,14 +1,23 @@
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Mutation, Resolver } from "@nestjs/graphql";
 import { Restaurant } from "./entities/restaurant.entitiy";
 
 @Resolver(()=>Restaurant)
 export class RestaurantResolver {
+    /* @Args("veganOnly") 는 GraphQL 의 Args 선언이고
+        veganOnly:boolean 이 TS-entity 를 위한 Args 선언이다.
+    */
     @Query(()=>[Restaurant])
     restaurants(@Args("veganOnly") veganOnly: boolean):Restaurant[]{
         console.log(veganOnly);
         return [];
     }
-    /* @Args("veganOnly") 는 GraphQL 의 Args 선언이고
-        veganOnly:boolean 이 TS-entity 를 위한 Args 선언이다.
-    */
+    @Mutation(()=>Boolean)
+    createRestaurant(
+        @Args("name") name: string,
+        @Args("isVegan") isVegan: boolean,
+        @Args("address") address: string,
+        @Args("ownerName") ownerName: string
+    ):boolean {
+        return true;
+    }
 }
